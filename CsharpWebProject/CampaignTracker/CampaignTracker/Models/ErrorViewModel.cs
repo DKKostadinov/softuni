ff4e480b756bea0934af665b799b80a2fc2140a4
DKKostadinov <kostadinov.dk@gmail.com>
2020-03-18 13:18:54 +0200

    Initial Commit

---
+++ CsharpWebProject/CampaignTracker/CampaignTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CampaignTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
