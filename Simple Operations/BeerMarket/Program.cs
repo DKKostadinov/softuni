bbb7b94b4b16b4feb890f442ec4139436ccf4d8a
DKKostadinov <kostadinov.dk@gmail.com>
2021-07-26 21:37:35 +0300

    initial commit

---
+++ Simple Operations/BeerMarket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BeerMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var whiskeyPrice = double.Parse(Console.ReadLine());
+            var beer = double.Parse(Console.ReadLine());
+            var wine = double.Parse(Console.ReadLine());
+            var rakia = double.Parse(Console.ReadLine());
+            var whiskey = double.Parse(Console.ReadLine());
+
+            var priceTotal = whiskey * whiskeyPrice + rakia * whiskeyPrice / 2 
+                + wine * whiskeyPrice / 2 * 0.6 + beer * whiskeyPrice / 2 * 0.2;
+
+            Console.WriteLine(String.Format("{0:0.00}", priceTotal));
+        }
+    }
+}
