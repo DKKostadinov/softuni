5727195fa06ebbbf625f84eb70ce11820a31242d
DKKostadinov <kostadinov.dk@gmail.com>
2021-07-26 19:22:52 +0300

    initial commit

---
+++ ProgrammingBasics/FirstSteps/StarSquare/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StarSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            for (int i = 0; i < side; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < side-2; i++)
+            {
+                Console.Write("*");
+                for (int j = 0; j < side-2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+            for (int i = 0; i < side; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
