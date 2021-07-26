cf15d2664953da72705bb64de76255873c8445c1
DKKostadinov <kostadinov.dk@gmail.com>
2021-07-26 18:47:29 +0300

    initial commit

---
+++ ProgrammingBasics/FirstSteps/RectArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RectArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the sides of the rectangle");
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area of the rectangle: " + sideA*sideB);
+
+        }
+    }
+}
