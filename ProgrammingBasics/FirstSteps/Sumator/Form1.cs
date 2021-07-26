d539c4cf12bed75a06d8ef7cb37571840ec46818
DKKostadinov <kostadinov.dk@gmail.com>
2021-07-26 19:33:02 +0300

    initial commit

---
+++ ProgrammingBasics/FirstSteps/Sumator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sumator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label3.Text = (double.Parse(textBox1.Text) + double.Parse(textBox2.Text)).ToString(); 
+            }
+            catch (Exception)
+            {
+                label3.Text = "error";
+            }
+        }
+    }
+}
