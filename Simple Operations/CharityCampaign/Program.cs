c8bcea3f595fb13f27920bb414d7d2f48e65d334
DKKostadinov <kostadinov.dk@gmail.com>
2021-07-26 21:17:10 +0300

    initial commit

--- Simple Operations/CharityCampaign/Program.cs
+++ Simple Operations/CharityCampaign/Program.cs
@@ -6,7 +6,13 @@ namespace CharityCampaign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var days = int.Parse(Console.ReadLine());
+            var bakers = int.Parse(Console.ReadLine());
+            var cakes = int.Parse(Console.ReadLine());
+            var waffles = int.Parse(Console.ReadLine());
+            var pancakes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(String.Format("{0:0.00}", days * bakers * (waffles * 5.8 + pancakes * 3.2 + cakes * 45) * 7 / 8));
         }
     }
 }
