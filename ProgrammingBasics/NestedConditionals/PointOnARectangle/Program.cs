b0ebbbe944c857640913004797fa55961af6eec8
DKKostadinov <kostadinov.dk@gmail.com>
2021-07-27 10:51:12 +0300

    initial commit

---
+++ ProgrammingBasics/NestedConditionals/PointOnARectangle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointOnARectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
